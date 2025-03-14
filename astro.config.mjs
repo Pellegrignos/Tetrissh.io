9b5a403acd097c08de6e6c9a1ce6965e92ce31b5
Pellegtignos <marco.pellegrino29.97@gmail.com>
2025-03-14 22:18:20 +0100

    modified web link in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-	site: https://Pellegrignos/Tetrissh.io
+	site: 'https://Pellegrignos.github.io/Tetrissh.io'
 });
